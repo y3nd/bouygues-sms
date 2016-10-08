1a12b99248fb02abaacc66c2ee142fd6bff24ca2
Yoann N <y3ndev@gmail.com>
2016-10-09 01:36:15 +0200

    1.1.0

--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 
 Package to use the Bouygues Telecom SMS unofficial API (5 SMS /day limitation)
 
-Based on [this php script](https://rpi-florentv.zapto.org/gitlist/index.php/send-sms.git/blob/master/bouygues/bouygues.php)
+Uses calls from [this page](https://www.secure.bbox.bouyguestelecom.fr/services/SMSIHD/sendSMS.phtml), and based on [this php script](https://rpi-florentv.zapto.org/gitlist/index.php/send-sms.git/blob/master/bouygues/bouygues.php)
 
 * ES6 (needs Node.JS >= 6.0.0)
 * 5 SMS /day
 * Quota reset at midnight
-* 160 chars count limit
+* 160 chars limit (message is truncated after the limit)
+* Error(s) management
+* Only ≈125 lines
 
 ## Usage
 ### Installation
@@ -19,21 +21,48 @@ npm install bouygues-sms
 ### Auth + send "Hello World!" to 0600000000
 ```javascript
 const Bouygues = require("bouygues-sms");
-var sms = new Bouygues("bouygueslogin", "bouyguespassword", 1);
+var sms = new Bouygues("bouygueslogin", "bouyguespassword", 1); // 3rd argument is for debug log (1 for enabled, 0 for disabled)
 
-sms.send("Hello World!", "0600000000", () => {
-  console.log("success");
+sms.send("Hello World!", "0600000000", (error) => {
+  if(error) {
+    console.log("An error occured: " + error.code)
+  } else {
+    console.log("success");
+  }
 });
 ```
 ### Get quota left
+**Left quota is checked at sms sending, no need to double check it (_don't put a sms.send in the getQuota callback_)**
 ```javascript
-sms.getQuota((quota) => {
-  console.log(quota + " sms left today");
+sms.getQuota((quota, error) => {
+  if(error) {
+    console.log("An error occured: " + error.code)
+  } else {
+    console.log(quota + " sms left today");
+  }
 })
 ```
 ### Send to multiple numbers (up to 5)
 ```javascript
-sms.send("Hello World!", ["0600000001", "0600000002", "0600000003", "0600000004", "0600000005"], () => {
-  console.log("success");
+sms.send("Hello World!", ["0600000001", "0600000002", "0600000003", "0600000004", "0600000005"], (error) => {
+  if(error) {
+    console.log("An error occured: " + error.code)
+  } else {
+    console.log("success");
+  }
 });
 ```
+### Error codes
+| Code             | Meaning                                                                    |
+|:----------------:|:--------------------------------------------------------------------------:|
+| LOGIN_UNKNOWN    | Login page has changed or Bouygues services are down                       |
+| LOGIN_WRONG      | Credentials are wrong                                                      |
+| QUOTA_EXCEEDED   | Quota is exceeded and SMS can't be sent                                    |
+| ERROR_GETQUOTA   | Error getting quota, page has changed or Bouygues services are down        |
+| SMS_CONFIRMATION | Error at SMS confirmation, page has changed or Bouygues services are down  |
+| SMS_RESULT       | Error at SMS result, page has changed or Bouygues services are down        |
+
+### Notes
+* **I'm NOT affiliated with Bouygues Telecom or one of its branches**
+* This module may not work if Bouygues change the service, then **please report it by creating an issue**
+* Emojis chars are replaced by a "?" by the Bouygues server ...
--- index.js
+++ index.js
@@ -12,50 +12,73 @@ module.exports = class {
     this.log("Authenticating..")
     this.req("GET", "https://www.mon-compte.bouyguestelecom.fr/cas/login", {}, (response, body) => {
 
-      var jsessionid = response.headers["set-cookie"][0].match(/JSESSIONID=(.*); Path=\/cas\/; HttpOnly/)[1]
-      this.log("Got jsessionid " + jsessionid);
+      var jsessionid = response.headers["set-cookie"][0].match(/JSESSIONID=(.*); Path=\/cas\/; HttpOnly/)
+      var lt = body.match(/<input type=\"hidden\" name=\"lt\" value=\"([a-zA-Z0-9_-]*)\"/);
 
-      var lt = body.match(/<input type=\"hidden\" name=\"lt\" value=\"([a-zA-Z0-9_-]*)\"/)[1];
-      this.log("Got lt value " + lt);
+      if(jsessionid == null && lt == null) cb({ code: "LOGIN_UNKNOWN" });
+      else {
+        var jsessionid = jsessionid[1];
+        this.log("Got jsessionid " + jsessionid);
+        var lt = lt[1];
+        this.log("Got lt value " + lt);
 
-      var postData = {
-        'username': this.userLogin,
-        'password': this.userPassword,
-        'rememberMe': 'true',
-        '_rememberMe': 'on',
-        'lt': lt,
-        'execution': 'e1s1',
-        '_eventId': 'submit'
+        var postData = {
+          'username': this.userLogin,
+          'password': this.userPassword,
+          'rememberMe': 'true',
+          '_rememberMe': 'on',
+          'lt': lt,
+          'execution': 'e1s1',
+          '_eventId': 'submit'
+        }
+        this.req("POST", "https://www.mon-compte.bouyguestelecom.fr/cas/login;jsessionid=" + jsessionid + "?service=https%3A%2F%2Fwww.secure.bbox.bouyguestelecom.fr%2Fservices%2FSMSIHD%2FsendSMS.phtml", postData, (response, body) => {
+          if(body.match(/<p class=\"color-mid-grey\">Votre identifiant ou votre mot de passe est incorrect<\/p>/) !== null) cb({ code: "LOGIN_WRONG" });
+          else {
+            this.log("Authenticated successfully!");
+            cb(false);
+          }
+        });
       }
-      this.req("POST", "https://www.mon-compte.bouyguestelecom.fr/cas/login;jsessionid=" + jsessionid + "?service=https%3A%2F%2Fwww.secure.bbox.bouyguestelecom.fr%2Fservices%2FSMSIHD%2FsendSMS.phtml", postData, (response, body) => {
-        this.log("Authenticated successfully!");
-        cb();
-      });
     });
   }
 
   send(msg, numbers, cb) {
-    this.login(() => {
-      this.getQuota((quota) => {
+    this.getQuota((quota, error) => {
+      if(error) cb(error);
+      else {
         var numberscount = 0;
         if(Array.isArray(numbers)) numberscount = numbers.length;
         if((quota-numberscount) >= 0) {
           this.sendSMS(msg, numbers, () => {
             this.log("SMS successfully sent!");
+            cb()
           });
         }
-        else this.log("Quota exceeded, message not sent");
-      })
-
-    });
+        else {
+          this.log("Quota exceeded, message not sent");
+          cb({ code: "QUOTA_EXCEEDED" })
+        }
+      }
+    })
   }
 
   getQuota(cb) {
-    this.req("GET", "https://www.secure.bbox.bouyguestelecom.fr/services/SMSIHD/sendSMS.phtml", {}, (response, body) => {
-      var quota = body.match(/Il vous reste <strong>(\d*) SMS gratuit\(s\)<\/strong>/)[1];
-      this.log(quota + "/5 message(s) left");
-      cb(quota);
-    })
+    this.login((error) => {
+      if(error) {
+        cb(false, error);
+      }
+      else {
+        this.req("GET", "https://www.secure.bbox.bouyguestelecom.fr/services/SMSIHD/sendSMS.phtml", {}, (response, body) => {
+          var quota = body.match(/Il vous reste <strong>(\d*) SMS gratuit\(s\)<\/strong>/);
+          if(quota == null) cb({ code: "ERROR_GETQUOTA" })
+          else {
+            var quota = quota[1];
+            this.log(quota + "/5 message(s) left");
+            cb(quota);
+          }
+        })
+      }
+    });
   }
 
   sendSMS(msg, numbers, cb) {
@@ -70,26 +93,28 @@ module.exports = class {
 
     var postdata = {
       'fieldMsisdn': numbers,
-      'fieldMessage': msg.slice(0, 161),
+      'fieldMessage': msg.slice(0, 160),
       'Verif.x': '51',
       'Verif.y': '16'
     }
 
     // Send confirmation
     this.req("POST", "https://www.secure.bbox.bouyguestelecom.fr/services/SMSIHD/confirmSendSMS.phtml", postdata, (response, body) => {
-      // Valid sending
-      this.req("GET", "https://www.secure.bbox.bouyguestelecom.fr/services/SMSIHD/resultSendSMS.phtml", {}, (response, body) => {
-        cb();
-      })
+      if(body.match(/<span class=\"titre\" style=\"float:left;\">Validation<\/span>/) == null) cb({ code: "SMS_CONFIRMATION" });
+      else {
+        // Valid sending
+        this.req("GET", "https://www.secure.bbox.bouyguestelecom.fr/services/SMSIHD/resultSendSMS.phtml", {}, (response, body) => {
+          if(body.match(/<td valign=\"top\">Votre message a bien été envoyé au numéro : <\/td>/) == null) cb({ code: "SMS_RESULT" });
+          else cb(false);
+        });
+      }
     })
   }
 
   req(method, url, data, cb) {
     this.log(method + " - " + url + " - " + JSON.stringify(data));
     request({ method: method, url: url, form: data }, (error, response, body) => {
-      if (!error) {
-        cb(response, body);
-      }
+      if (!error) cb(response, body);
     })
   }
 
